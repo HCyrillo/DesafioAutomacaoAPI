26bb05687a9de202152f9c5aabb64deacf88b339
HCyrillo <45167942+HCyrillo@users.noreply.github.com>
2023-10-20 03:00:43 -0300

    Atualizar o BaseUtils.java

--- DesafioAutomacaoAPI/src/test/java/utils/BaseUtils.java
+++ DesafioAutomacaoAPI/src/test/java/utils/BaseUtils.java
@@ -5,11 +5,6 @@ import io.restassured.response.Response;
 public class BaseUtils {
 	public Response response;
 	public String endpointURL = "https://teste/1.0/elegibilidade_cliente";
-	public String indicadorElegibilidadeCliente;
-	public String clienteCorrentista;
-	public String contaAtiva;
-	public String tipoProduto;
-	public int codigoProdutoOrigem;
 	public String body;
 	public String contentType = "application/json";
 	public String accept = "application/json";
